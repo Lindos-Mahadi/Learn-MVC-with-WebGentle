056fab9c9c8ecaade5d0b430298c7a2d7dfac68d
Lindos-Mahadi <lindosmahadi@.com>
2021-06-25 12:53:08 +0600

    Added Simple View without Model

--- Controllers/PracticeController.cs
+++ Controllers/PracticeController.cs
@@ -9,34 +9,14 @@ namespace WebApp_MVC_AllTopics_with_WebGentle.Controllers
     public class PracticeController : Controller
     {
         // GET: Practice
-        public string Index()
+        public ActionResult Index()
         {
-            return "Practice Controller";
+            return View();
         }
-        public string Name()
+        public ViewResult AboutUs()
         {
-            return "Hello Lindos Mahadi";
+            return View();
         }
-        public string Profile(int id)
-        {
-            string profile = string.Empty;
-            if (id == 1)
-            {
-                profile = "Profile ID is 1";
-            }
-            else if (id == 2)
-            {
-                profile = "Profile ID is 2";
-            }
-            else if (id == 3)
-            {
-                profile = "Profile ID is 3";
-            }
-            return profile;
-        }
-        public string Address( int id, string department)
-        {
-            return "id = " + id +" "+ "Department =" + department;
-        }
+
     }
 }
