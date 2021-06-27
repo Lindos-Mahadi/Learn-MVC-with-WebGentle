cb5a2122fed6d215be289593fb4031ebd3caa607
Lindos-Mahadi <lindosmahadi@.com>
2021-06-27 21:39:02 +0600

    Custom HTML helpers in MVC 5 with Static Method

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -12,42 +12,11 @@ namespace WebApp_MVC_AllTopics_with_WebGentle.Controllers
     public class EmployeesController : Controller
     {
         // GET: Employees
-        //public ActionResult Index()
-        //{
-
-        //    return View();
-        //}
-        //[HttpPost]
         public ActionResult Index()
-        {
-            Employee emp = new Employee()
-            {
-                Id = 1,
-                FullName = "Lindos",
-                EmailAddress = "lindos@gmail",
-                Gender = "Male",
-                PersonalWebSite = "lindos.com",
-            };
-            return View(emp);
-        }
-
-        public ActionResult EditField()
-        {
-            Employee emp = new Employee()
-            {
-                Id = 1,
-                FullName = "Lindos",
-                EmailAddress = "lindos@gmail",
-                Gender = "Male",
-                PersonalWebSite = "lindos.com",
-            };
-            return View(emp);
-        }
-        [HttpPost]
-        public ActionResult EditField(Employee emp)
         {
             
             return View();
         }
+
     }
 }
