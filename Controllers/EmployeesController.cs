80b0a7608e9adc5763dce60862f29a04938824c8
Lindos-Mahadi <lindosmahadi@.com>
2021-06-28 12:50:56 +0600

    HTTP verbs in MVC 5 POST, GET, PUT, PATCH, and DELETE

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -18,6 +18,13 @@ namespace WebApp_MVC_AllTopics_with_WebGentle.Controllers
             TempData["MyName"] = "Temp Data From Index Method";
             return View();
         }
+        // HTTP verbs  POST, GET, PUT, PATCH, and DELETE
+        [HttpPost]
+        [HttpGet]
+        [HttpDelete]
+        [HttpPut]
+        [HttpHead]
+        [HttpOptions]
         public ActionResult Index2()
         {
             ViewBag.MyName = TempData["MyName"];
