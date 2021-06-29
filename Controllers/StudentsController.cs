9cb120aa8f4f5792174beb9c588aba2eeb3e478b
Lindos-Mahadi <lindosmahadi@.com>
2021-06-29 12:37:39 +0600

    Pass data from view to controller in mvc 5

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -8,100 +8,18 @@ using WebApp_MVC_AllTopics_with_WebGentle.Models;
 
 namespace WebApp_MVC_AllTopics_with_WebGentle.Controllers
 {
-    [RoutePrefix("students")]
     public class StudentsController : Controller
     {
         // GET: Students
-        [Route("")]
-        public ActionResult GetAllStudent()
+        [HttpGet]
+        public ActionResult Index()
         {
-            var students = Students();
-            return View(students);
+            return View();
         }
-
-        // GET: Students/Details/5
-        [Route("{id:int}")]
-        public ActionResult GetStudent(int id)
+        [HttpPost]
+        public string StudentName(string firstName, string lastName)
         {
-            var students = Students().FirstOrDefault(x => x.StudentID == id);
-            return View(students);
-        }
-        [Route("{id}")]
-        public string MyString(string id)
-        {
-            return id;
-        }
-        [Route("{id}/address")]
-        public ActionResult GetStudentAddress(int id)
-        {
-            var studentAddress = Students().Where(x => x.StudentID == id).Select(x => x.StudentAddress).FirstOrDefault();
-            return View(studentAddress);
-        }
-        private List<Student> Students()
-        {
-            return new List<Student>()
-            {
-                new Student()
-                {
-                    StudentID = 1,
-                    StudentName = "Lindos",
-                    Standard = "First",
-                    StudentAddress = new StudentAddress()
-                    {
-                        HomeNumber = "10",
-                        City = "Dhaka",
-                        State = "Dhaka division"
-                    },
-                },
-                new Student()
-                {
-                    StudentID = 2,
-                    StudentName = "Lindos 2",
-                    Standard = "Second",
-                    StudentAddress = new StudentAddress()
-                    {
-                        HomeNumber = "1010",
-                        City = "Khulna",
-                        State = "Khulna division"
-                    },
-                },
-                new Student()
-                {
-                    StudentID = 3,
-                    StudentName = "Lindos 3",
-                    Standard = "Four",
-                    StudentAddress = new StudentAddress()
-                    {
-                        HomeNumber = "101020",
-                        City = "Gazipur",
-                        State = "Dhaka division"
-                    },
-                },
-                new Student()
-                {
-                    StudentID = 5,
-                    StudentName = "Lindos 5",
-                    Standard = "Five",
-                    StudentAddress = new StudentAddress()
-                    {
-                        HomeNumber = "101030",
-                        City = "Dinazpur",
-                        State = "Dinazpur division"
-                    },
-                },
-                new Student()
-                {
-                    StudentID = 5,
-                    StudentName = "Lindos 6",
-                    Standard = "Six",
-                    StudentAddress = new StudentAddress()
-                    {
-                        HomeNumber = "101040",
-                        City = "Joshor",
-                        State = "Joshor division"
-                    },
-                },
-            };
+            return "Form Parameters-" +firstName + ", "+ lastName ;
         }
     }
 }
