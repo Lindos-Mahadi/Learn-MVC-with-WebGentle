590e322e9e8ac60d476890d1192ba3aa2ff1e89d
Lindos-Mahadi <lindosmahadi@.com>
2021-07-01 19:04:57 +0600

    Save Data in Database in MVC 5

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,27 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyApp.Db.DbOperations;
+using MyModels.Models;
 
 namespace WebApp_MVC_AllTopics_with_WebGentle.Controllers
 {
     public class HomeController : Controller
     {
+        EmployeeRepository repository=null;
+
+        public HomeController()
+        {
+            repository = new EmployeeRepository();
+        }
         public ActionResult Index()
         {
+            var dataList = repository.ToString();
+            return View(dataList);
+        }
+        public ActionResult Create()
+        {
             return View();
         }
-
-        public ActionResult About()
+        public ActionResult CreateEmployee()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
-
-        public ActionResult Contact()
+        [HttpPost]
+        public ActionResult CreateEmployee(EmployeeModel model)
         {
-            ViewBag.Message = "Your contact page.";
-
+            if (ModelState.IsValid)
+            {
+                int id = repository.AddEmployee(model);
+                if (id > 0)
+                {
+                    ModelState.Clear();
+                    ViewBag.IsSuccess = "Data Added";
+                }
+            }
             return View();
         }
     }
