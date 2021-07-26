d346b8cf43b30a358e2c1c28cc1ddb5f9d5724c7
Lindos-Mahadi <lindosmahadi@.com>
2021-07-26 13:14:41 +0600

    Ajax BeginForm with newly installed content and script files in MVC-5

---
+++ Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using MyModels.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp_MVC_AllTopics_with_WebGentle.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult Index(EmployeeModel model)
+        {
+            //return View();
+            string message = "SUCCESS";
+            return Json(new { Message = message,  JsonRequestBehavior.AllowGet });
+        }
+    }
+}
