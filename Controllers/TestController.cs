95a9f1d40c9acfbf595cbfd67b4460c427cddd97
Lindos-Mahadi <lindosmahadi@.com>
2021-07-26 19:52:33 +0600

    Ajax ActionLink in MVC-5

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -1,4 +1,5 @@
 ﻿using MyModels.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +21,55 @@ namespace WebApp_MVC_AllTopics_with_WebGentle.Controllers
             //return View();
             string message = "SUCCESS";
             return Json(new { Message = message,  JsonRequestBehavior.AllowGet });
+        }
+
+        public JsonResult About()
+        {
+            List<string> countries = new List<string>()
+            {
+                "Afghanistan"
+                ,"Albania"
+                ,"Algeria"
+                ,"Andorra"
+                ,"Angola"
+                ,"Antigua and Barbuda"
+                ,"Argentina"
+                ,"Armenia"
+                ,"Australia"
+                ,"Austria"
+                ,"Austrian Empire"
+                ,"Azerbaijan"
+            }; 
+            var json = JsonConvert.SerializeObject(countries);
+            return Json(json, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult Learn()
+        {
+            List<string> countries = new List<string>()
+            {
+                "Cabo Verde"
+                ,"Cambodia"
+                ,"Cameroon"
+                ,"Canada"
+                ,"Cayman Islands"
+                ,"Central African Republic"
+                ,"Central American Federation"
+                ,"Chad"
+                ,"Chile"
+                ,"China"
+                ,"Colombia"
+                ,"Comoros"
+                ,"Congo Free State"
+                ,"Costa Rica"
+                ,"Cote d’Ivoire (Ivory Coast)"
+                ,"Croatia"
+                ,"Cuba"
+                ,"Cyprus"
+                ,"Czechia"
+                ,"Czechoslovakia"
+            };
+            var json = JsonConvert.SerializeObject(countries);
+            return Json(json, JsonRequestBehavior.AllowGet);
         }
     }
 }
