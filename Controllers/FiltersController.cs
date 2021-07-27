b0af59ac66585066d38e5da3825b2ca5981376f0
Lindos-Mahadi <lindosmahadi@.com>
2021-07-27 11:16:53 +0600

    OurputCach Filter in MVC-5

---
+++ Controllers/FiltersController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp_MVC_AllTopics_with_WebGentle.Controllers
+{
+    public class FiltersController : Controller
+    {
+        // GET: Filters
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // OUTPUT CACH FILER
+        [OutputCache(Duration = 20)] // duration always works in second
+        public ActionResult GetDate()
+        {
+            return View();
+        }
+    }
+}
